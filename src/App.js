219fb9c87fd7beba09f8d905406b4c63f332b9c9
TulioLeitao <tulioleitao@gmail.com>
2022-10-18 19:37:44 -0300

    finalização do exercicio A031

--- src/App.js
+++ src/App.js
@@ -17,7 +17,34 @@ export default function App() {
           <input type="text" placeholder="Busca" id="campoDeBusca" />
         </header>
 
-        <CardVideo/>
+        <main>
+          <nav className="menu-vertical">
+            <ul>
+              <li className="botoes-meunu-vertical">Início</li>
+              <li className="botoes-meunu-vertical">Em alta</li>
+              <li className="botoes-meunu-vertical">Inscrições</li>
+              <hr />
+              <li className="botoes-meunu-vertical">Originais</li>
+              <li className="botoes-meunu-vertical">Histórico</li>
+            </ul>
+            </nav>
+            <section className="painel-de-videos">
+            <CardVideo/>
+            <CardVideo/>
+            <CardVideo/>
+            <CardVideo/>
+            <CardVideo/>
+            <CardVideo/>
+            <CardVideo/>
+            <CardVideo/>            
+            </section>
+            
+        
+        
+        
+        </main>
+
+        
 
         <footer>
           <h4>Oi! Eu moro no footer!</h4>
--- src/componentes/cardVideo.js
+++ src/componentes/cardVideo.js
@@ -13,61 +13,15 @@ const CardVideo = () => { //criamos a função arrow function aqui.
        
     return (//todas as fuções e variáveis tem que vir em acima do return. 
         
-        <main>
-          <nav className="menu-vertical">
-            <ul>
-              <li className="botoes-meunu-vertical">Início</li>
-              <li className="botoes-meunu-vertical">Em alta</li>
-              <li className="botoes-meunu-vertical">Inscrições</li>
-              <hr />
-              <li className="botoes-meunu-vertical">Originais</li>
-              <li className="botoes-meunu-vertical">Histórico</li>
-            </ul>
-          </nav>
-
-          <section className="painel-de-videos">            
+        
+                    
             <div className="box-pagina-principal" onClick={reproduzVideo}>
               <InfoUsuario/>
               <img src="https://picsum.photos/400/400?a=1 " alt="" />
               <h4>{titulo}</h4>
-            </div>
-            <div className="box-pagina-principal" onClick={reproduzVideo}>
-              <InfoUsuario/>
-              <img src="https://picsum.photos/400/400?a=2 " alt="" />
-              <h4>{titulo}</h4>
-            </div>
-            <div className="box-pagina-principal" onClick={reproduzVideo}>
-              <InfoUsuario/>
-              <img src="https://picsum.photos/400/400?a=3 " alt="" />
-              <h4>{titulo}</h4>
-            </div>
-            <div className="box-pagina-principal" onClick={reproduzVideo}>
-              <InfoUsuario/>
-              <img src="https://picsum.photos/400/400?a=4 " alt="" />
-              <h4>{titulo}</h4>
-            </div>
-            <div className="box-pagina-principal" onClick={reproduzVideo}>
-              <InfoUsuario/>
-              <img src="https://picsum.photos/400/400?a=5 " alt="" />
-              <h4>{titulo}</h4>
-            </div>
-            <div className="box-pagina-principal" onClick={reproduzVideo}>
-              <InfoUsuario/>
-              <img src="https://picsum.photos/400/400?a=6 " alt="" />
-              <h4>{titulo}</h4>
-            </div>
-            <div className="box-pagina-principal" onClick={reproduzVideo}>
-              <InfoUsuario/>
-              <img src="https://picsum.photos/400/400?a=7 " alt="" />
-              <h4>{titulo}</h4>
-            </div>
-            <div className="box-pagina-principal" onClick={reproduzVideo}>
-              <InfoUsuario/>
-              <img src="https://picsum.photos/400/400?a=8 " alt="" />
-              <h4>{titulo}</h4>
-            </div>
-          </section>
-        </main>
+            </div>            
+          
+        
     
     )
 }
