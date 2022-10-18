c86ea723c759c93d5572b49832bcb059ff605e71
TulioLeitao <tulioleitao@gmail.com>
2022-10-18 18:30:44 -0300

    exercício que eu achei que tinha entregado, e não entreguei

---
+++ src/componentes/cardVideo.js
@@ -0,0 +1,77 @@
+import React from "react";
+import InfoUsuario from "./infoUsuario";
+import '../componentes/cardVideo.css';
+
+const CardVideo = () => { //criamos a função arrow function aqui. 
+    
+    const titulo = "Título do vídeo";
+
+    function reproduzVideo() {
+        alert("O vídeo está sendo reproduzido");
+  }
+    
+       
+    return (//todas as fuções e variáveis tem que vir em acima do return. 
+        
+        <main>
+          <nav className="menu-vertical">
+            <ul>
+              <li className="botoes-meunu-vertical">Início</li>
+              <li className="botoes-meunu-vertical">Em alta</li>
+              <li className="botoes-meunu-vertical">Inscrições</li>
+              <hr />
+              <li className="botoes-meunu-vertical">Originais</li>
+              <li className="botoes-meunu-vertical">Histórico</li>
+            </ul>
+          </nav>
+
+          <section className="painel-de-videos">            
+            <div className="box-pagina-principal" onClick={reproduzVideo}>
+              <InfoUsuario/>
+              <img src="https://picsum.photos/400/400?a=1 " alt="" />
+              <h4>{titulo}</h4>
+            </div>
+            <div className="box-pagina-principal" onClick={reproduzVideo}>
+              <InfoUsuario/>
+              <img src="https://picsum.photos/400/400?a=2 " alt="" />
+              <h4>{titulo}</h4>
+            </div>
+            <div className="box-pagina-principal" onClick={reproduzVideo}>
+              <InfoUsuario/>
+              <img src="https://picsum.photos/400/400?a=3 " alt="" />
+              <h4>{titulo}</h4>
+            </div>
+            <div className="box-pagina-principal" onClick={reproduzVideo}>
+              <InfoUsuario/>
+              <img src="https://picsum.photos/400/400?a=4 " alt="" />
+              <h4>{titulo}</h4>
+            </div>
+            <div className="box-pagina-principal" onClick={reproduzVideo}>
+              <InfoUsuario/>
+              <img src="https://picsum.photos/400/400?a=5 " alt="" />
+              <h4>{titulo}</h4>
+            </div>
+            <div className="box-pagina-principal" onClick={reproduzVideo}>
+              <InfoUsuario/>
+              <img src="https://picsum.photos/400/400?a=6 " alt="" />
+              <h4>{titulo}</h4>
+            </div>
+            <div className="box-pagina-principal" onClick={reproduzVideo}>
+              <InfoUsuario/>
+              <img src="https://picsum.photos/400/400?a=7 " alt="" />
+              <h4>{titulo}</h4>
+            </div>
+            <div className="box-pagina-principal" onClick={reproduzVideo}>
+              <InfoUsuario/>
+              <img src="https://picsum.photos/400/400?a=8 " alt="" />
+              <h4>{titulo}</h4>
+            </div>
+          </section>
+        </main>
+    
+    )
+}
+
+
+export default CardVideo; //chama a função para o mundo.
+
